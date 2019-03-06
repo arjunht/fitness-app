c7f1ce1ae142639945e986ff56131a09bf49cd57
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-03-05 19:22:48 -0600

    Style Slider, Date and Button

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
+import { purple } from '../utils/colors'
 
-const TextButton = ({ onPress, children }) => {
+const TextButton = ({ onPress, children, style = {} }) => {
 	return (
 		<TouchableOpacity onPress={onPress}>
-			<Text>{children}</Text>
+			<Text style={[{ textAlign: 'center', color: purple} , style]}>{children}</Text>
 		</TouchableOpacity>
 	)
 }

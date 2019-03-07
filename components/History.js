2bbdb43af2e1a570394c664c2a1b70872dbe4d97
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-03-06 21:51:31 -0600

    Display calendar and associated metrics for each day

--- components/History.js
+++ components/History.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { fetchCalendarResults } from '../utils/api'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
 
 class History extends Component {
 	
@@ -22,11 +23,35 @@ class History extends Component {
 			})
 	}
 	
-	render() {
+	renderItem = ({ today, ...metrics }, formattedDate, key) => (
+		<View>
+			{today
+				? <Text>{JSON.stringify(today)}</Text>
+				: <Text>{JSON.stringify(metrics)}</Text>}
+		</View>
+	)
+	
+	renderEmptyDate(formattedDate) {
 		return (
 			<View>
-				<Text>{JSON.stringify(this.props)}</Text>
+				<Text>No Data for this day</Text>
 			</View>
+		)
+	}
+	
+	renderEmptyDate = () => {
+		
+	}
+	
+	render() {
+		const { entries } = this.props
+		
+		return (
+			<UdaciFitnessCalendar
+				items={entries}
+				renderItem={this.renderItem}
+				renderEmptyDate={this.renderEmptyDate}
+			/>
 		)
 	}
 }
